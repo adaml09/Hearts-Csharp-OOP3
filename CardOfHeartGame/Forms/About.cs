af1bc0af852fb2dac987e36b9c49e67e4203cb35
VP7698 <156264403+VP7698@users.noreply.github.com>
2024-04-18 23:21:23 -0400

    Add files via upload

---
+++ CardOfHeartGame/Forms/About.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace GROUP.Game
+{
+	/// <summary>
+	/// Summary description for About.
+	/// </summary>
+	public class About : System.Windows.Forms.Form
+	{
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private RichTextBox richTextBox1;
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.Container components = null;
+
+		public About()
+		{
+			//
+			// Required for Windows Form Designer support
+			//
+			InitializeComponent();
+
+			//
+			// TODO: Add any constructor code after InitializeComponent call
+			//
+		}
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{
+				if(components != null)
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}		
+
+		private void button1_Click(object sender, System.EventArgs e)
+		{
+			Close();
+		}
+
+        #region Windows Form Designer generated code
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(206, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(215, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Rules";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button1.Location = new System.Drawing.Point(283, 656);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "OK";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(8, 77);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(650, 572);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // About
+            // 
+            this.AutoScaleBaseSize = new System.Drawing.Size(8, 19);
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(582, 553);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "About";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "The Card Game Of Hearts";
+            this.Load += new System.EventHandler(this.About_Load);
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+
+        private void About_Load(object sender, EventArgs e)
+        {
+            richTextBox1.Text = @"This 2-player card game aims to collect the fewest points possible. 
+Each heart card is worth 1 point, and the queen of spades is worth 13 points. 
+Players pass cards in specific patterns before each round. 
+Hearts can't be played until broken, and in the first round, hearts and 
+the queen of spades cannot be led. 
+The game ends when a player reaches 100 points or more, with 
+the player with the fewest points winning. 
+If a player collects all hearts and the queen of spades, 
+they get 0 points, while others receive penalty points.";
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
